dcff4d07c41ebbc1c8111564e91d671318e80204
AbSattar <mdabdullsattar@gmail.com>
2024-04-13 03:45:25 +0530

    Icons done

--- stinder/App.jsx
+++ stinder/App.jsx
@@ -1,32 +1,56 @@
-import React from 'react';
-import {View, StyleSheet, Image, useWindowDimensions} from 'react-native';
+import React, {useState} from 'react';
+import {View, StyleSheet, Pressable, SafeAreaView} from 'react-native';
 import {
   GestureHandlerRootView,
 } from 'react-native-gesture-handler';
 import HomeScreen from './src/screens/HomeScreen';
 import MatchesScreen from './src/screens/MatchesScreen';
+import Octicons from 'react-native-vector-icons/Octicons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 
 
 const App = () => {  
-
-
-
+  const [activeScreen, setActiveScreen] = useState('HOME');
+  const color = '#b5b5b5';
+  const activeColor = '#F76C6B';
     return (
       <GestureHandlerRootView style={{flex: 1}}>
-        <View style={styles.pageContainer}>
-          <MatchesScreen />       
-        </View>
+        <SafeAreaView style={{flex: 1}}>
+         <View style={styles.pageContainer}>
+           <View style={styles.topNavigation}>
+            <Pressable onPress={() => setActiveScreen('HOME')}>
+            <Octicons name="home" size={25} color={activeScreen === 'HOME' ? activeColor : color} />
+            </Pressable>
+           <MaterialCommunityIcons name="star-four-points" size={24} color={color} />
+            <Pressable onPress={() => setActiveScreen('CHAT')}>
+            <Ionicons name="chatbubbles-outline"size={24} color={activeScreen === 'CHAT' ? activeColor : color} />
+            </Pressable>
+            <FontAwesome name="user" size={24} color={color} />
+            </View>  
+           {activeScreen === 'HOME' && <HomeScreen />}
+           {activeScreen === 'CHAT' && <MatchesScreen />}
+         </View>
+        </SafeAreaView>
       </GestureHandlerRootView>
     );
 };
 
 const styles = StyleSheet.create({
- pageContainer: {
+  pageContainer: {
     justifyContent: 'center', 
     alignItems: 'center', 
     flex: 1,
- },
+  },
+ topNavigation: {
+  flexDirection: 'row',
+  justifyContent: 'space-around',
+  width: '100%',
+  paddingTop: 10,
+
+ }
 });
 
 export default App;
--- stinder/src/screens/HomeScreen.js
+++ stinder/src/screens/HomeScreen.js
@@ -3,6 +3,9 @@ import {View, StyleSheet } from 'react-native';
 import Card from '../components/TinderCard';
 import users from '../../assets/data/users';
 import AnimatedStack from '../components/AnimatedStack';
+import Entypo from 'react-native-vector-icons/Entypo';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const HomeScreen = () => {  
 
@@ -11,7 +14,7 @@ const HomeScreen = () => {
   };
 
   const onSwipeRight = (user) => {
-    console.warn("swipe right:", user.name)
+    console.warn("swipe right: ", user.name)
   };
 
 
@@ -20,7 +23,28 @@ const HomeScreen = () => {
         <AnimatedStack data={users} renderItem={(({ item }) => <Card user={item} />)}
         onSwipeLeft={onSwipeLeft}
         onSwipeRight={onSwipeRight}
-        />         
+        />      
+         <View style={styles.icons}>
+         <View style={styles.button}>
+          <FontAwesome name="undo" size={24} color="#FBD88B" />
+          </View>
+
+          <View style={styles.button}>
+          <Entypo name="cross" size={24} color="#F76C6B" />
+          </View>
+
+          <View style={styles.button}>
+          <FontAwesome name="star" size={24} color="#3AB4CC" />
+          </View>
+
+          <View style={styles.button}>
+          <FontAwesome name="heart" size={24} color="#4FCC94" />
+          </View>
+
+          <View style={styles.button}>
+          <Ionicons name="flash" size={24} color="#A65CD2" />
+          </View>
+          </View>   
         </View>
     );
 };
@@ -31,6 +55,22 @@ const styles = StyleSheet.create({
     alignItems: 'center', 
     flex: 1,
     width: '100%',
+    backgroundColor: '#ededed',
+ },
+ icons: {
+  flexDirection: 'row',
+  justifyContent: 'space-around',
+  width: '100%',
+  padding: 10,
+ },
+ button: {
+  width: 50,
+  height: 50,
+  justifyContent: 'center',
+  alignItems: 'center',
+  backgroundColor: 'white',
+  padding: 10,
+  borderRadius: 50,
  },
 });
 
